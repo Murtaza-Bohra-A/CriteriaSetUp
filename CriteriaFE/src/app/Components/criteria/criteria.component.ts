0643b6e4b9a5fa2eaa17bf567a18d18ff078c6d9
Murtaza-Bohra-A <murtaza7249885185@gmail.com>
2025-08-23 19:10:10 +0530

    Improment in Criteria Page

--- CriteriaFE/src/app/Components/criteria/criteria.component.ts
+++ CriteriaFE/src/app/Components/criteria/criteria.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-criteria',
-  imports: [],
+  imports: [FormsModule, InputGroupModule, InputGroupAddonModule, InputTextModule, FloatLabelModule, ButtonModule],
   templateUrl: './criteria.component.html',
   styleUrl: './criteria.component.scss'
 })
 export class CriteriaComponent {
-
+  value: string=""
 }
