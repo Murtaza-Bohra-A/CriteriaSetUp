a195c4386ce46cc2de024ff9ca40c1e42616dd06
Your Name <you@example.com>
2025-09-01 15:54:54 +0530

    Adding Criteria Table

--- CriteriaFE/src/app/Components/criteria/criteria.component.ts
+++ CriteriaFE/src/app/Components/criteria/criteria.component.ts
@@ -6,12 +6,34 @@ import { InputTextModule } from 'primeng/inputtext';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-criteria',
-  imports: [FormsModule, InputGroupModule, InputGroupAddonModule, InputTextModule, FloatLabelModule, ButtonModule],
+  imports: [FormsModule, InputGroupModule, InputGroupAddonModule, InputTextModule, FloatLabelModule, ButtonModule, TableModule, CommonModule],
   templateUrl: './criteria.component.html',
   styleUrl: './criteria.component.scss'
 })
 export class CriteriaComponent {
-  value: string=""
+  Source: string = ""
+  Criteria: string = ""
+  srNo: number = 0;
+  products: any[] = [];
+
+  loadCriteriatable() {
+   this.products = [
+      {
+        name: "Murtaza",
+        code: 1234,
+        category: "Demo",
+        quantity: 1
+      }, {
+        name: "Murtaza",
+        code: 1234,
+        category: "Demo",
+        quantity: 1
+      }
+    ]
+  }
 }
